e6256d78d92a2983f3df5457cc6a7154b53f96c8
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-12 06:51:28 +0800

    356. using reactTransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -8,6 +9,7 @@ import List from './components/List/List';
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -23,6 +25,33 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>React Animations</h1>
+        <button
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                opacity: state === 'entered' ? 1 : 0,
+                transition: 'opacity 300ms ease-out',
+              }}
+            ></div>
+          )}
+        </Transition>
         <Modal show={modalIsOpen} closed={this.closeModal} />
         <Backdrop show={modalIsOpen} />
         <button className='Button' onClick={this.showModal}>
